c405492ec31d300017a20014b1c5c9dabab9424a
oussama <oussama@oussama-pc.wireless-1x.unc.edu>
2019-08-29 21:41:52 -0400

    New submission.

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,36 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		int numofcustomers = scan.nextInt();
+		
+		
+		for (int i = 0; i < numofcustomers; i++) {
+			String fname = scan.next();
+			
+			String finitial = fname.charAt(0) + ".";
+			
+			String lname = scan.next();
+			
+			String fullname = finitial + " " + lname;
+			
+			int numofitems = scan.nextInt();
+			double totalcost = 0;
+			for (int j = 0; j < numofitems; j++) {
+				int quantity = scan.nextInt();
+				scan.next();
+				double cost = scan.nextDouble();
+				totalcost = calculateCostOfItem(quantity, cost);
+			}
+			System.out.println(fullname + ": " + totalcost);
+		}
+		
+		
 	}
+	static double calculateCostOfItem(int quant, double price) {
+		for (int counter = 1; counter < quant; counter++) {
+			price = price + price;
+		}
+		return price;
+	}
+	
 }
